a6c8a37268b0381bcaf7faccd01c4ce9e1a2a62d
lenny.bakkalian@gmail.com <lenny.bakkalian@gmail.com>
2020-03-27 17:42:26 +0100

    adding req, res & exception callback

--- src/io/sslprox/api/SSLProxApi.java
+++ src/io/sslprox/api/SSLProxApi.java
@@ -10,6 +10,7 @@ import io.sslprox.requests.LoginRequest;
 import io.sslprox.requests.Req;
 import io.sslprox.requests.project.GetCurrentUserRequest;
 import io.sslprox.responses.LoginResponse;
+import io.sslprox.responses.Res;
 import io.sslprox.responses.UserResponse;
 import kong.unirest.HttpRequest;
 import kong.unirest.HttpResponse;
@@ -21,12 +22,26 @@ public class SSLProxApi {
 	private String session;
 	private UserResponse currentUser;
 
+	private ApiEvent event;
+
 	public SSLProxApi(String host) {
 		this.host = host;
 	}
 
+	public void setEvent(ApiEvent event) {
+		this.event = event;
+	}
+
+	public ApiEvent getEvent() {
+		return event;
+	}
+
 	public void setSession(String session) {
 		this.session = session;
+		if (session == null) {
+			currentUser = null;
+			return;
+		}
 		try {
 			currentUser = call(new GetCurrentUserRequest());
 		} catch (Exception e) {
@@ -45,57 +60,75 @@ public class SSLProxApi {
 
 	@SuppressWarnings("unchecked")
 	public <T> T call(Req<?> req) throws Exception {
-		HttpRequest<?> request = null;
-		Request annotation = null;
-		for (Constructor<?> c : req.getClass().getConstructors())
-			if (c.isAnnotationPresent(Request.class))
-				annotation = c.getAnnotation(Request.class);
-		if (annotation == null)
-			throw new Exception(
-					"Annotation \"Request\" in constructor " + req.getClass().getSimpleName() + " not found!");
+		String bodyStr = null;
+		try {
+			HttpRequest<?> request = null;
+			Request annotation = null;
+			for (Constructor<?> c : req.getClass().getConstructors())
+				if (c.isAnnotationPresent(Request.class))
+					annotation = c.getAnnotation(Request.class);
+			if (annotation == null)
+				throw new Exception(
+						"Annotation \"Request\" in constructor " + req.getClass().getSimpleName() + " not found!");
 
-		String url = host + annotation.path();
+			String url = host + annotation.path();
 
-		switch (annotation.method().name().toLowerCase()) {
-		case "post":
-			request = Unirest.post(url).body(req);
-			break;
-		case "put":
-			request = Unirest.put(url).body(req);
-			break;
-		case "delete":
-			request = Unirest.delete(url).body(req);
-			break;
-		case "get":
-			request = Unirest.get(url);
-			break;
-		default:
-			System.out.println("undefined method in SSLProxApi: " + annotation.method());
-			return null;
+			switch (annotation.method().name().toLowerCase()) {
+			case "post":
+				request = Unirest.post(url).body(req);
+				break;
+			case "put":
+				request = Unirest.put(url).body(req);
+				break;
+			case "delete":
+				request = Unirest.delete(url).body(req);
+				break;
+			case "get":
+				request = Unirest.get(url);
+				break;
+			default:
+				System.out.println("undefined method in SSLProxApi: " + annotation.method());
+				return null;
+			}
+			request = request.header("x-sslprox-session", session);
+			request.header("Content-Type", "application/json");
+			Class<?> clazz = ((Class
+					.forName(((ParameterizedType) req.getClass().getGenericSuperclass()).getActualTypeArguments()[0]
+							.toString().split(" ")[1])));
+
+			ObjectMapper mapper = new ObjectMapper();
+
+			if (event != null)
+				event.onReq(req);
+
+			HttpResponse<String> res = request.asString();
+
+			bodyStr = res.getBody();
+
+			if (bodyStr.contains("invalid_session"))
+				throw new Exception("Invalid Session");
+
+			// System.out.println(bodyStr);
+
+			// System.out.println(request.getBody().get().toString());
+
+			T body = (T) mapper.readValue(bodyStr, clazz);
+
+			// autosave session
+			if (req instanceof LoginRequest && ((LoginResponse) body).status == LoginResponse.SUCCESS) {
+				LoginResponse lRes = (LoginResponse) body;
+				setSession(lRes.session);
+			}
+
+			if (event != null)
+				event.onRes(req, body, bodyStr);
+
+			return body;
+		} catch (Exception e) {
+			if (event != null)
+				event.onErr(req, bodyStr, e);
+			throw e;
 		}
-		request = request.header("x-sslprox-session", session);
-		request.header("Content-Type", "application/json");
-		Class<?> clazz = ((Class
-				.forName(((ParameterizedType) req.getClass().getGenericSuperclass()).getActualTypeArguments()[0]
-						.toString().split(" ")[1])));
-
-		ObjectMapper mapper = new ObjectMapper();
-
-		HttpResponse<String> res = request.asString();
-
-		String bodyStr = res.getBody();
-
-		// System.out.println(bodyStr);
-
-		T body = (T) mapper.readValue(bodyStr, clazz);
-
-		// autosave session
-		if (req instanceof LoginRequest) {
-			LoginResponse lRes = (LoginResponse) body;
-			setSession(lRes.session);
-		}
-		return body;
-
 	}
 
 	public void async(Req<?> req, SSLProxResHandler handler) {
@@ -106,6 +139,14 @@ public class SSLProxApi {
 				e.printStackTrace();
 			}
 		}).start();
+	}
+
+	public static abstract class ApiEvent {
+		public abstract void onReq(Req<?> req);
+
+		public abstract <T> void onRes(Req<?> req, T res, String rawRes);
+
+		public abstract void onErr(Req<?> req, String res, Exception ex);
 	}
 
 }
